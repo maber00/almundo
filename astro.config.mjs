bc850b86b17d34affde7dc53af00f920cb0de286
maber00 <mbedoyarudas@gmail.com>
2025-04-13 12:13:28 -0500

    “Astro679”

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 import icon from "astro-icon";
-
 
 export default defineConfig({
   integrations: [
@@ -13,5 +12,5 @@ export default defineConfig({
     }),
   ],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
